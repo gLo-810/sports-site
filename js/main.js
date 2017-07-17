07236865f22d707ba0bb10d9e42f6a32c8474a24
Gabe <glopez810@gmail.com>
2017-07-16 22:21:50 -0500

    add football images, names array. need to random the names separate from teams

--- js/main.js
+++ js/main.js
@@ -1,16 +1,73 @@
 var button = document.getElementById("button");
 var reset = document.getElementById("reset");
 var tDisplay = document.getElementById("parent");
+var userNames = ["Mayme",  
+"Keeley",  
+"Gilbert",  
+"Willette", 
+"Stacee",  
+"Melynda",  
+"Jerri",  
+"Hassan", 
+"Sage",  
+"Melisa",  
+"Lawana",  
+"Era",  
+"Venus",  
+"Salena", 
+"Eleni",  
+"Raymond",  
+"Fredricka",  
+"Arianna",  
+"Dexter",  
+"Adella",  
+"Bethanie",  
+"Jonna",  
+"Kizzie",  
+"Dominica",  
+"Renee",  
+"Annamaria",  
+"Ivette",  
+"Londa",  
+"Marnie",  
+"Erika"  ];
 
-// create container containing teams
+// baseball teams
+baseball.addEventListener("click", function baseball() {
+  tDisplay.innerHTML = "";
 for (var i = 1; i <= 30; i++) {
   var newDiv = document.createElement("div");
   var newImg = document.createElement("img");
+  var userName = document.createElement("p");
+
   newDiv.className = "col-sm-2"
   newDiv.appendChild(newImg);
+  newDiv.appendChild(userName);
+  userName.textContent = userNames[Math.random() * i | 0];
   newImg.src = "images\\baseball\\team" + i + ".jpg";
   tDisplay.appendChild(newDiv);
 }
+});
+
+// football football
+football.addEventListener("click", function football() {
+  tDisplay.innerHTML = "";
+
+  for (var i = 1; i <= 32; i++) {
+    var newDiv = document.createElement("div");
+    var nestDiv = document.createElement("div"); // NEED TO NEST div within div to separate user names from teams 
+    var newImg = document.createElement("img");
+    var userName = document.createElement("p");
+
+    newDiv.className = "col-sm-2"
+    newDiv.appendChild(newImg);
+    newDiv.appendChild(nestDiv);
+    nestDiv.appendChild(userName);
+    userName.textContent = userNames[Math.random() * i | 0];
+    newImg.src = "images\\football\\team" + i + ".gif";
+    tDisplay.appendChild(newDiv);
+  }
+});
 
 // random the images var i = tDisplay.children.length; i >= 0; i--
 button.addEventListener("click", function random() {
@@ -18,6 +75,7 @@ button.addEventListener("click", function random() {
     tDisplay.appendChild(tDisplay.children[Math.random() * i | 0]);
   }
 });
+
 
 // reset images to default
 reset.addEventListener("click", function reset() {
@@ -31,64 +89,4 @@ reset.addEventListener("click", function reset() {
     newImg.src = "images\\baseball\\team" + i + ".jpg";
     tDisplay.appendChild(newDiv);
   }
-
-  // document.getElementById("parent").innerHTML = "";
-  // for (var i = 0; i < originalChildren.length; i++) {
-  //   document.getElementById("parent").appendChild(originalChildren[i]);
-  // }
 });
-
-
-
-// var divWimg = newDiv.appendChild(newImg);
-
-// var team1 = document.getElementById("team1");
-// var team2 = document.getElementById("team2");
-// var team3 = document.getElementById("team3");
-// var team4 = document.getElementById("team4");
-// var team5 = document.getElementById("team5");
-// var team6 = document.getElementById("team6");
-
-
-// var pics = ["team1.jpg", "team2.jpg", "team3.jpg", "team4.jpg", "team5.jpg", "team6.jpg"];
-
-// tDisplay.appendChild(newDiv);
-//
-// newDiv.appendChild(newImg);
-
-//
-// var teams = [team1, team2, team3, team4, team5, team6];
-
-
-// button.addEventListener("click", function random() {
-//
-// //   var i = pics.length,
-// //     rNum, temp;
-// //
-// //   // shuffle pictures using fisher yates
-// //   while (--i > 0) {
-// //     rNum = Math.floor(Math.random() * (i + 1));
-// //     temp = pics[rNum];
-// //     pics[rNum] = pics[i];
-// //     pics[i] = temp;
-// //   }
-// //
-//   // display images
-//   for (var i = 0; i < teams.length; i++) {
-//     teams[i].src = "images\\sports\\" + pics[i];
-//   }
-//
-//
-// });
-
-
-// reset.addEventListener("click", function reset() {
-//
-//
-//   // team1.src = "images\\sports\\team1.jpg";
-//   // team2.src = "images\\sports\\team2.jpg";
-//   // team3.src = "images\\sports\\team3.jpg";
-//   // team4.src = "images\\sports\\team4.jpg";
-//   // team5.src = "images\\sports\\team5.jpg";
-//   // team6.src = "images\\sports\\team6.jpg";
-// });
