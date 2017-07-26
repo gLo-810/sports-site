cf09aef579c27ba686455fead80c1defe47f9567
Gabriel Lopez <gabe@Gabriels-MacBook-Pro.local>
2017-07-25 21:23:16 -0500

    added functionality for football random. random is still not true, needs work on spaces added to names array

--- js/main.js
+++ js/main.js
@@ -29,24 +29,23 @@ for (var i = 1; i <= 30; i++) {
 });
 
 // // football football
-// football.addEventListener("click", function football() {
-//   tDisplay.innerHTML = "";
-//
-//   for (var i = 1; i <= 32; i++) {
-//     var newDiv = document.createElement("div");
-//     var nestDiv = document.createElement("div"); // NEED TO NEST div within div to separate user names from teams
-//     var newImg = document.createElement("img");
-//     var userName = document.createElement("p");
-//
-//     newDiv.className = "col-sm-2"
-//     newDiv.appendChild(newImg);
-//     newDiv.appendChild(nestDiv);
-//     nestDiv.appendChild(userName);
-//     userName.textContent = names[Math.random() * i | 0];
-//     newImg.src = "images\\football\\team" + i + ".gif";
-//     tDisplay.appendChild(newDiv);
-//   }
-// });
+  football.addEventListener("click", function football() {
+
+    tDisplay.innerHTML = "";
+
+    for (var i = 1; i <= 32; i++) {
+      var newDiv = document.createElement("div");
+      var newImg = document.createElement("img");
+      var userName = document.createElement("p");
+
+      newDiv.className = "col-sm-3 col-md-3 col-lg-2"
+      newDiv.appendChild(newImg);
+      newDiv.appendChild(userName);
+      userName.textContent =  names[Math.random() * i | 0];
+      newImg.src = "images\\football\\team" + i + ".gif";
+      tDisplay.appendChild(newDiv);
+    }
+  });
 
 // random the images var i = tDisplay.children.length; i >= 0; i--
 button.addEventListener("click", function random() {
