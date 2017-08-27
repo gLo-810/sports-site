00434b713beb84137e685af7a07845f5bca2b9a5
Gabe <glopez810@gmail.com>
2017-08-27 17:43:34 -0500

    removed old commented code, still duplicates

--- js/main.js
+++ js/main.js
@@ -10,57 +10,18 @@ function saveNames() {
   names = textarea.value.split('\n');
 }
 
+// save names to array, no submit button
 textarea.addEventListener('blur', saveNames, false);
 
-// baseball teams
-// baseball.addEventListener("click", function baseball() {
-//   //   tDisplay.innerHTML = "";
-//   // for (var i = 1; i <= 30; i++) {
-//   //   var newDiv = document.createElement("div");
-//   //   var newImg = document.createElement("img");
-//   //   var userName = document.createElement("p");
-//   //
-//   //   newDiv.className = "col-sm-3 col-md-3 col-lg-2"
-//   //   newDiv.appendChild(newImg);
-//   //   newDiv.appendChild(userName);
-//   //   userName.textContent =  names[Math.random() * i | 0];
-//   //   newImg.src = "images\\baseball\\team" + i + ".jpg";
-//   //   tDisplay.appendChild(newDiv);
-//
-//
-// });
 
-//});
-
-// // football football
-// football.addEventListener("click", function football() {
-//
-//   tDisplay.innerHTML = "";
-//
-//   for (var i = 1; i <= 32; i++) {
-//     var newDiv = document.createElement("div");
-//     var newImg = document.createElement("img");
-//     var userName = document.createElement("p");
-//
-//     newDiv.className = "col-sm-3 col-md-3 col-lg-2"
-//     newDiv.appendChild(newImg);
-//     newDiv.appendChild(userName);
-//     userName.textContent =  names[Math.random() * i | 0];
-//     newImg.src = "images\\football\\team" + i + ".gif";
-//     tDisplay.appendChild(newDiv);
-//   }
-// });
-
-//create elements
-
-
+//button behavoir
 bGroup.addEventListener("click", function images(e) {
 
 
   if (e.target.id !== "random") {
     tDisplay.innerHTML = "";
 
-    for (var i = 1; i <=names.length; i++) {
+    for (var i = 0; i < names.length; i++) {
       var newDiv = document.createElement("div");
       var newImg = document.createElement("img");
       var userName = document.createElement("p");
@@ -68,7 +29,7 @@ bGroup.addEventListener("click", function images(e) {
       newDiv.className = "col-sm-3 col-md-3 col-lg-2"
       newDiv.appendChild(newImg);
       newDiv.appendChild(userName);
-      userName.textContent = names[Math.random() * i | 0];
+      userName.textContent = names[i];
 
 
       if (e.target.id === "baseball") {
@@ -80,34 +41,12 @@ bGroup.addEventListener("click", function images(e) {
     };
   } else {
     for (var i = 1; i <= tDisplay.children.length; i++) {
-      tDisplay.appendChild(tDisplay.children[Math.random() * i | 0]);
-      tDisplay.getElementsByTagName("p")[Math.random() * i | 0].textContent = names[Math.random() * i | 0];
+      var tdLength = tDisplay.children.length;
+      tDisplay.appendChild(tDisplay.children[Math.random() * tdLength | 0]);
+      tDisplay.getElementsByTagName("p")[Math.random() * tdLength | 0].textContent = names[Math.random() * tdLength | 0];
+
     }
   }
 
 
 });
-
-
-// random the images
-// random.addEventListener("click", function random() {
-//   for (var i = 0; i < tDisplay.children.length; i++) {
-//     tDisplay.appendChild(tDisplay.children[Math.random() * i | 0]);
-//     tDisplay.getElementsByTagName("p")[Math.random() * i | 0].textContent = names[Math.random() * i | 0];
-//   }
-// });
-
-
-// // reset images to default
-// reset.addEventListener("click", function reset() {
-//   tDisplay.innerHTML = "";
-//
-//   for (var i = 1; i <= 30; i++) {
-//     var newDiv = document.createElement("div");
-//     var newImg = document.createElement("img");
-//     newDiv.className = "col-sm-1"
-//     newDiv.appendChild(newImg);
-//     newImg.src = "images\\baseball\\team" + i + ".jpg";
-//     tDisplay.appendChild(newDiv);
-//   }
-// });
