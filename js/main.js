23d71e17e65b17822dafdbe3868b6e1e2a65963c
Gabe <glopez810@gmail.com>
2017-08-28 22:55:35 -0500

    added shuffle function and changed baseball and football buttons to radio buttons

--- js/main.js
+++ js/main.js
@@ -47,6 +47,11 @@ bGroup.addEventListener("click", function images(e) {
 
     }
   }
-
-
 });
+
+function shuffle(a) {
+  for (let i = a.length; i; i--) {
+    let j = Math.floor(Math.random() * i);
+    [a[i - 1], a[j]] = [a[j], a[i - 1]];
+  }
+}
